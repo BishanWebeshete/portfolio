58a7b696314eb27a6d18b2ae80191e041c231c61
Bishan Webeshete <webesheteb@gmail.com>
2023-11-09 07:24:01 -0800

    updated home page

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
